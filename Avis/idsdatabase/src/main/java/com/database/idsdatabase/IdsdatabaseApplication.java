a3fa52450d6f32987e2d5660b248b07de81ab351
Leo <leonardo.mogianesi@studenti.unicam.it>
2019-11-27 16:50:10 +0100

    PORCA DI QUELLA TROIA FUNZIONA

---
+++ Avis/idsdatabase/src/main/java/com/database/idsdatabase/IdsdatabaseApplication.java
@@ -0,0 +1,28 @@
+package com.database.idsdatabase;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class IdsdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsdatabaseApplication.class, args);
+	}
+
+	@Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User();
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+
+}
---
+++ Avis/idsdatabase/src/main/java/com/database/idsdatabase/User.java
@@ -0,0 +1,53 @@
+package com.database.idsdatabase;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private String name;
+
+    private String password;
+
+    private String emailUtente;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+        this.name = name;
+	}
+
+	public String getpassword() {
+		return password;
+	}
+
+	public void setpassword(String password) {
+        this.password = password;
+	}
+
+	public String getEmailUtente() {
+		return emailUtente;
+	}
+
+	public void setEmailUtente(String emailUtente) {
+		this.emailUtente = emailUtente;
+	}
+
+
+}
