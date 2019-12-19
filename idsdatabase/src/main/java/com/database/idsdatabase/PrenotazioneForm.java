bd2e09265a91d9e7b881323a91610615b1992293
Leo <leonardo.mogianesi@studenti.unicam.it>
2019-12-19 16:16:17 +0100

    Tolto alcune cose inutili

---
+++ idsdatabase/src/main/java/com/database/idsdatabase/PrenotazioneForm.java
@@ -0,0 +1,81 @@
+package com.database.idsdatabase;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+
+public class PrenotazioneForm{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+
+    private String centro;
+
+    private String compleanno;
+
+    private String telefono;
+
+    private String note;
+
+    public String getcentro() {
+        return centro;
+    }
+    
+    public void setcentro(final String centro) {
+        this.centro = centro;
+    }
+
+    public String getcompleanno() {
+        return compleanno;
+    }
+    
+    public void setcompleanno(final String compleanno) {
+        this.compleanno = compleanno;
+    }
+
+    public String gettelefono() {
+        return telefono;
+    }
+    
+    public void settelefono(final String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getnote() {
+        return note;
+    }
+    
+    public void setnote(final String note) {
+        this.note = note;
+    }
+
+    @ManyToOne
+    private Donazione data;
+
+    public Donazione getdata(){
+		return data;
+	}
+
+	public void setdata(Donazione data){
+		this.data = data;
+    }
+    
+    @ManyToOne
+    private Donazione ora;
+
+    public Donazione getora(){
+		return ora;
+	}
+
+	public void setora(Donazione ora){
+		this.ora = ora;
+    }
+
+    
+
+
+}
--- idsdatabase/src/main/java/com/database/idsdatabase/User.java
+++ idsdatabase/src/main/java/com/database/idsdatabase/User.java
@@ -22,8 +22,6 @@ public class User {
 
 	private String password;
 
-	private boolean donazioneEmergenza;
-
 	public User(){
 
 	}
