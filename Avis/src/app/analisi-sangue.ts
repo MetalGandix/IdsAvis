057c19551baee8fac1abf98a588136f16089e89b
lorenzo498 <56250667+lorenzo498@users.noreply.github.com>
2019-12-21 09:36:32 +0100

    analisiSangue

---
+++ Avis/src/app/analisi-sangue.ts
@@ -0,0 +1,3 @@
+export class AnalisiSangue {
+    id: number;
+}
---
+++ idsdatabase/src/main/java/com/database/idsdatabase/AnalisiSangueRepository.java
@@ -0,0 +1,9 @@
+package com.database.idsdatabase;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.database.idsdatabase.AnalisiSangue;
+
+public interface AnalisiSangueRepository extends JpaRepository<AnalisiSangue, Long> {
+
+}
