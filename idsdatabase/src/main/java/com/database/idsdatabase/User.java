96f9b8661549916d836e810a3a026908884deeed
Leo <leonardo.mogianesi@studenti.unicam.it>
2020-01-03 17:02:45 +0100

    LA S

--- idsdatabase/src/main/java/com/database/idsdatabase/User.java
+++ idsdatabase/src/main/java/com/database/idsdatabase/User.java
@@ -10,106 +10,106 @@ import javax.persistence.ManyToOne;
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;
-	
-	private String name;
+ private long id;
+ 
+ private String name;
 
-	private String lastname;
+ private String lastname;
 
-	private String email;
-	
-	private String stato;
+ private String email;
+ 
+ private String stato;
 
-	private String password;
+ private String password;
 
-	private boolean donazioneEmergenza;
+ private boolean donazioneEmergenza;
 
-	public User(){
+ public User(){
 
-	}
+ }
 
-	public User(final long id, final String name, final String lastname, final String email, final String stato, final String password, final Role ruolo, final boolean donazioneEmergenza) {
-		this.id = id;
-		this.name = name;
-		this.lastname = lastname;
-		this.email = email;
-		this.stato = stato;
-		this.password = password;
-		this.ruolo = ruolo;
-		this.donazioneEmergenza = donazioneEmergenza;
-	}
+ public User(final long id, final String name, final String lastname, final String email, final String stato, final String password, final Role ruolo, final boolean donazioneEmergenza) {
+  this.id = id;
+  this.name = name;
+  this.lastname = lastname;
+  this.email = email;
+  this.stato = stato;
+  this.password = password;
+  this.ruolo = ruolo;
+  this.donazioneEmergenza = donazioneEmergenza;
+ }
 
-	public long getid() {
-		return id;
-	}
+ public long getid() {
+  return id;
+ }
 
-	public void setid(final Long id) {
-		this.id = id;
-	}
+ public void setid(final Long id) {
+  this.id = id;
+ }
 
-	public String getname() {
-		return name;
-	}
+ public String getname() {
+  return name;
+ }
 
-	public void setname(final String name) {
+ public void setname(final String name) {
         this.name = name;
-	}
+ }
 
-	@Override
-	public String toString(){
-		return "" + id + "" + name + "" + lastname + "" + stato + "" + email + "" + password + "" + ruolo.getid() + "" + ruolo.getrole() ;
-	}
+ @Override
+ public String toString(){
+  return "" + id + "" + name + "" + lastname + "" + stato + "" + email + "" + password + "" + ruolo.getid() + "" + ruolo.getrole() ;
+ }
 
-	public String getemail() {
-		return email;
-	}
+ public String getemail() {
+  return email;
+ }
 
-	public void setemail(final String email) {
-		this.email = email;
-	}
+ public void setemail(final String email) {
+  this.email = email;
+ }
 
-	public String getpassword() {
-		return password;
-	}
+ public String getpassword() {
+  return password;
+ }
 
-	public void setpassword(final String password) {
+ public void setpassword(final String password) {
         this.password = password;
-	}
+ }
 
-	public String getstato() {
-		return stato;
-	}
+ public String getstato() {
+  return stato;
+ }
 
-	public void setstato(final String stato) {
+ public void setstato(final String stato) {
         this.stato = stato;
-	}
+ }
 
-	public String getlastname() {
-		return lastname;
-	}
+ public String getlastname() {
+  return lastname;
+ }
 
-	public void setlastname(final String lastname) {
-		this.lastname = lastname;
-	}
+ public void setlastname(final String lastname) {
+  this.lastname = lastname;
+ }
 
-	public boolean getdonazioneEmergenza() {
-		return donazioneEmergenza;
-	}
+ public boolean getdonazioneEmergenza() {
+  return donazioneEmergenza;
+ }
 
-	public void setdonazioneEmergenza(final boolean donazioneEmergenza) {
-		this.donazioneEmergenza = donazioneEmergenza;
-	}
+ public void setdonazioneEmergenza(final boolean donazioneEmergenza) {
+  this.donazioneEmergenza = donazioneEmergenza;
+ }
 
-	@ManyToOne
-	private Role ruolo;
-	
-	public Role getRuolo(){
-		return ruolo;
-	}
+ @ManyToOne
+ private Role ruolo;
+ 
+ public Role getRuolo(){
+  return ruolo;
+ }
 
-	public void setRuolo(Role ruolo){
-		this.ruolo = ruolo;
-	}
+ public void setRuolo(Role ruolo){
+  this.ruolo = ruolo;
+ }
 
 
 }
