60cdb4856444019c1df371ec3ae81b0dab7bf171
Leo <leonardo.mogianesi@studenti.unicam.it>
2020-01-03 16:25:51 +0100

    Revert "analisiSangue"
    This reverts commit 057c19551baee8fac1abf98a588136f16089e89b.

--- idsdatabase/src/main/java/com/database/idsdatabase/AnalisiSangueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.database.idsdatabase;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.database.idsdatabase.AnalisiSangue;
-
-public interface AnalisiSangueRepository extends JpaRepository<AnalisiSangue, Long> {
-
-}
