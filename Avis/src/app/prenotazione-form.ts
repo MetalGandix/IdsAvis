e0545854dc6fe1e876b887f80b9dcaa695a21e41
Leo <leonardo.mogianesi@studenti.unicam.it>
2020-01-03 17:12:21 +0100

    commit errato da reversare (FUNZIONA PRENOTAZIONE FORM)

--- Avis/src/app/prenotazione-form.ts
+++ Avis/src/app/prenotazione-form.ts
@@ -1,5 +1,8 @@
 export class PrenotazioneForm {
+<<<<<<< HEAD
     id: number
+=======
+>>>>>>> parent of 0e04424... Capito alcune cose riguardanti i controller, però mi manda tutti NULL
     centro: string;
     compleanno: string;
     telefono: string;
