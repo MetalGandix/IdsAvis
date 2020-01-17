00a130dd483de2cee1fc9158230743518f1246fa
Leo <leonardo.mogianesi@studenti.unicam.it>
2020-01-17 10:40:56 +0100

    DELETE WORKING

--- Avis/src/app/avis.service.ts
+++ Avis/src/app/avis.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { OrarioDonazione } from './orario-donazione'
 import { catchError } from 'rxjs/operators';
 
@@ -14,9 +14,6 @@ const httpOptions = {
 
 @Injectable()
 export class AvisService {
-
-
-  handleError: any;
  
 
   constructor(private http: HttpClient) {}
@@ -32,13 +29,26 @@ export class AvisService {
      return this.http.post<OrarioDonazione>(this.avisUrl, orarioDonazione);
    }
 
-    deleteOrario(id): Observable<{OrarioDonazione}> {
+   public deleteOrario(id: number): Observable<{}> {
     const url = `${this.avisUrl}/${id}`;
-    return this.http.delete(url, httpOptions)
-    .pipe(
-      catchError(this.handleError("deleteOrario"))
-    );
-  }
+    return this.http.delete(url, httpOptions);
+   }
+
+    private handleError(error: HttpErrorResponse) {
+      if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      // return an observable with a user-facing error message
+      return throwError(
+        'Something bad happened; please try again later.');
+    };
 }
 
 
