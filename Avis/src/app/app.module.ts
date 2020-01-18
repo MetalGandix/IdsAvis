51306a8b5ed70aedb31d9c9d04a135aeb6e9d813
Leo <leonardo.mogianesi@studenti.unicam.it>
2020-01-18 12:15:13 +0100

    Revert "eliminata pag. sede avis"
    This reverts commit 2678a4a5b63bdff09a27070a71b033577deb6611.

--- Avis/src/app/app.module.ts
+++ Avis/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { FormDonatoreComponent } from './form-donatore/form-donatore.component';
 import { InfoDonazioneComponent } from './info-donazione/info-donazione.component';
 import { SangueComponent } from './sangue/sangue.component';
 import { DonareComponent } from './donare/donare.component';
+import { SedeComponent } from './sede/sede.component';
 import { FooterComponent } from './footer/footer.component';
 import { AvisPageComponent } from './avis-page/avis-page.component';
 import { UserListComponent } from './user-list/user-list.component';
@@ -70,6 +71,7 @@ import { AuthenticationService } from './service/authentication.service';
     InfoDonazioneComponent,
     SangueComponent,
     DonareComponent,
+    SedeComponent,
     FooterComponent,
     AvisPageComponent,
     UserListComponent,
@@ -108,6 +110,7 @@ import { AuthenticationService } from './service/authentication.service';
       { path: 'info-donazione', component: InfoDonazioneComponent},
       { path: 'sangue', component: SangueComponent},
       { path: 'donare', component: DonareComponent},
+      { path: 'sede', component: SedeComponent},
       { path: 'user-form', component: UserFormComponent},
       { path: 'chi-puo-donare', component: ChiPuoDonareComponent},
       { path: 'come-donare', component: ComeDonareComponent},
