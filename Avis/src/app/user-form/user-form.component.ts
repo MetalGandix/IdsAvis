65dc3aa15a02bebddc294f1cf7489f2c40579462
Leo <leonardo.mogianesi@studenti.unicam.it>
2020-01-24 12:45:02 +0100

    Provando a fare qualcosa con i ruol

--- Avis/src/app/user-form/user-form.component.ts
+++ Avis/src/app/user-form/user-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../user-service.service'
 import { User } from '../user';
+import { AuthenticationService } from '../service/authentication.service';
 
 
 @Component({
@@ -14,10 +15,12 @@ export class UserFormComponent {
   user: User;
   showMsg: boolean = false;
   userExist: boolean = false;
+  
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private authenticate: AuthenticationService,
     private userService: UserService) {
     this.user = new User();
   }
@@ -26,6 +29,14 @@ export class UserFormComponent {
     this.userService.save(this.user).subscribe(data =>{this.showMsg= true;
     })
   }
+
+  mettiRuolo(){
+    this.authenticate.getRole().authority=='ROLE_UTENTE'
+  }
+
+
+
+
 
   existUser(){
     if(this.user.username!=''){
